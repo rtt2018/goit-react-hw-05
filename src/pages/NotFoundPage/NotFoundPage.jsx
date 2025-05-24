e2954d75bdc542651a68ef2d93ee58bc0629b2d8
Taras <r.taras.t.1@gmail.com>
2025-05-24 17:07:44 +0300

    final v.1

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -7,7 +7,7 @@ export default function NotFoundPage() {
   return (
     <div className={styles.container}>
       <Link to={'/'} className={styles.backLink}>
-        <MdOutlineArrowBackIos className={styles.backLinkSvg} /><p className={styles.desc}>404....  Йой! Шо сі стало? Тут ніц нема ))) </p>
+        <MdOutlineArrowBackIos className={styles.backLinkSvg} /><p className={styles.desc}>404....  Йой! Тут нічого немає. Повернутись на головну </p>
       </Link>
     </div>
   );
